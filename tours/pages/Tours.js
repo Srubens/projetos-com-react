91ad5d5a196cc8c0676a15778e0e9af62653e730
Rubens Filipe <rubensfilipe6@gmail.com>
2023-01-09 12:31:05 -0300

    ajustando e finalizando o projeto tours

--- tours/pages/Tours.js
+++ tours/pages/Tours.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Tours = ({image, price, title, paragrafo}) =>{
+const Tours = ({id, image, price, title, paragrafo, removerItem}) =>{
+    
+    const [readMore, setReadMore] = useState(false)
+
     return(
-        <article className='tour_sigle d-flex flex-column align-items-center col' >
-            <img src={image} alt="image" />
-            <div className="tour-info">
-                <h4>{title}</h4>
-                <span>{price}</span>
-                <p>{paragrafo}</p>
+        <article className='tour_sigle' >
+            <div className="d-flex flex-column align-items-center col">
+                <img src={image} alt="image" />
+                <div className="tour-info">
+                    <h4>{title}</h4>
+                    <span>{price}</span>
+                    <p>
+                    { readMore ? paragrafo : `${paragrafo.substring(0,140)}...` }
+                        <span className='d-flex justify-content-md-center flex-column align-items-center mt-2' >
+                            <span className="btn btn-primary col col-md-8" onClick={() => setReadMore(!readMore)} >
+                                { readMore ? 'Mostrar menos' : 'Leia mais' }
+                            </span>
+                        </span>
+                    </p>
+                    <div className="d-flex justify-content-md-center flex-column align-items-center">
+                        <div className="btn btn-outline-danger col col-md-8" onClick={() => removerItem(id)} >Remover item</div>
+                    </div>
+                </div>
             </div>
         </article>
     )
--- tours/pages/index.js
+++ tours/pages/index.js
@@ -5,36 +5,43 @@ import Tours from './Tours'
 const Index = () =>{
 
     const [tours, setTours] = useState(data)
-
+    
     const removerItem = (id) =>{
         let newTours = tours.filter((tour) => tour.id !== id)
-        setTours(newTours)
+        return setTours(newTours)
+    }
+
+    if( tours.length === 0 ){
+        return(
+            <>
+                <div className="d-flex text-center justify-content-md-center mt-2">
+                            <h1>Todos os locais foram retirados</h1>
+                        </div>
+                <div className="d-flex  justify-content-md-center">
+                    <div className='btn btn-primary' onClick={() => setTours(data)} >Recarregar</div>
+                </div>
+            </>
+        )
     }
 
     return(
         <>
             <main>
                 <div>
-                    <div className="d-flex">
-                        <h1>Locais turisticos para viagens pelo brasil</h1>
+                    <div className="d-flex text-center justify-content-md-center mt-2">
+                        <h1>Locais turisticos para viagens pelo Brasil</h1>
                     </div>
                     <div className="container">
 
                         <div className="row d-flex flex-column flex-md-row col gap-3 ">
                             {
                                 tours.map((tour, index) =>{
-                                    const { id, image, price, title, paragrafo } = tour
                                     return(
                                         <div className='tours col d-flex flex-column align-items-center' key={index} >
                                                 <Tours 
-                                                 image={tour.image}
-                                                 price={tour.price}
-                                                 title={tour.title}
-                                                 paragrafo={tour.paragrafo}
-                                                 key={index} />
-                                                <div className="btn btn-primary col-8 " >Ler mais</div>
-                                                <br/>
-                                                <div className="btn btn-outline-danger col-8" onClick={() => removerItem(id)} >remover item</div>
+                                                    {...tour}
+                                                    removerItem={removerItem}
+                                                 />
                                         </div>
                                     )
                                 })
