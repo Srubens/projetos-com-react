5b576cd7837bf2e913d72829b12d751e6e8beefa
Rubens Filipe <rubensfilipe6@gmail.com>
2023-01-18 21:21:02 -0300

    ajustando imagens estaticas

--- birthdayReminder/src/list-people.js
+++ birthdayReminder/src/list-people.js
@@ -1,26 +1,26 @@
 export const listPeople = [
     {
-        image:`http://localhost:3000/pessoa-1.jpg`,
+        image:`birthdayreminder-seven.vercel.app/pessoa-1.jpg`,
         name:'Maria Alice',
         age:'24 years'
     },
     {
-        image:`http://localhost:3000/pessoa-2.jpg`,
+        image:`birthdayreminder-seven.vercel.app/pessoa-2.jpg`,
         name:'Roberto Bagio',
         age:'30 years'
     },
     {
-        image:`http://localhost:3000/pessoa-3.jpg`,
+        image:`birthdayreminder-seven.vercel.app/pessoa-3.jpg`,
         name:'Bernardo Silva',
         age:'28 years'
     },
     {
-        image:`http://localhost:3000/pessoa-4.jpg`,
+        image:`birthdayreminder-seven.vercel.app/pessoa-4.jpg`,
         name:'Beatriz Lins',
         age:'25 years'
     },
     {
-        image:`http://localhost:3000/pessoa-5.jpg`,
+        image:`birthdayreminder-seven.vercel.app/pessoa-5.jpg`,
         name:'Clarice Fernandes',
         age:'18 years'
     },
