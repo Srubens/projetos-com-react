2d63cbc9c0f096eb276e849ceca9c199fd052659
Rubens Filipe <rubensfilipe6@gmail.com>
2023-01-30 15:46:14 -0300

    add modificacoes

--- menu/pages/_app.js
+++ menu/pages/_app.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Layout from './Components/Layout'
 import 'bootstrap/dist/css/bootstrap.css'
-import "../../css/style.css"
+import "../css/style.css"
 
 const MyApp = ({Component, pageProps}) =>{
     return(
